24a8b115f34842881ea36e3b498b505511edd814
Batsaikhanshdee <chbaagii96@gmail.com>
2021-03-10 19:33:51 +0800

    state bolon searc controller nemlee

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,29 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
+// Web app төлөв
+// Хайлтын query үр дүн 
+// Тухайн үзүүлж байгаа жор
+// Лайкласан жорууд
+// Захиалж байгаа найрлаганууд
 
-search.doSearch().then(r => console.log(r));
+const state = {};
+const controlSearch = async () => {
+    // Вэбээс хайлтын түлхүүр үгийг гаргаж авна.
+    const query = 'pizza';
+    if(query){
+        // Шинээр хайлтын обьектийг үүсгэж өгнө.
+        state.Search = new Search(query);
+        // Хайлт хийх зориулж дэлгэцийн UI бэлтгэнэ.
+        // Хайлтыг гүйцэтгэнэ.
+        await state.Search.doSearch();
+        // Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+        console.log(state.Search.result);
+    }
+
+};
+
+document.querySelector(".search").addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
